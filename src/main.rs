d57c64f4e5e52ee8a36d44fa3ca63a34491d6a22
Serega <26937163+serega-fighter@users.noreply.github.com>
2024-03-05 21:13:28 +0400

    async main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use rocket::{Build, Rocket};
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
     "Hello, Rocket!"
 }
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![index]).launch().await;
 }
